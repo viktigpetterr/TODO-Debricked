a1b97dc34e15b27c9652e221df953f3c078c2246
viktigpetterr <viktor.pettersson@debricked.com>
2020-10-11 19:04:47 +0200

    Updated db structure with new models and migrations.

---
+++ app/database/migrations/2020_10_11_164823_create_todo_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->boolean('done')
+                ->default(false)
+                ->nullable(false);
+            $table->text('text')
+                ->nullable(false);
+            $table->bigInteger('category_id')
+                ->unsigned()
+                ->nullable(false);
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+}
